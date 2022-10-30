2f28b8cc7a479813825acd76f4690ebe3887cb8e
bulan506 <116318596+bulan506@users.noreply.github.com>
2022-10-30 15:14:13 -0600

    nueva forma main
    se modifica la main para que tenga más funcionalidad con el panel de logeo, sino era asi no podia cerrar esa ventana tan facil.

--- ProyectoGato 2.0/src/proyectogato/Main.java
+++ ProyectoGato 2.0/src/proyectogato/Main.java
@@ -1,8 +1,5 @@
 package proyectogato;
-
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -11,21 +8,12 @@ import javafx.stage.Stage;
  */
 
 public class Main extends Application {
-    
     @Override
     public void start(Stage primaryStage) {
         Interface iE = new Interface();
-        
-        primaryStage.setScene(iE.welcomeScene());
-        primaryStage.setTitle("Juego del Gato");
-        primaryStage.getIcons().add(new Image("cat icon.png"));
-        primaryStage.show();
+        iE.logeo();
     }
-
-  
     public static void main(String[] args) {
         launch(args);
-        
     }
-    
 }
